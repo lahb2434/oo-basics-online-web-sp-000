789c21fb62adaed5711f0371bb46e08d0ccbd008
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-21 21:33:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,11 @@
 
 class Shoe 
   
-  attr_accessor :color, :size, :material
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   
-  def initialize(title)
-    @title = title
-    @page_count = 0
+  def initialize(brand)
+    @brand = brand
   end
 end
