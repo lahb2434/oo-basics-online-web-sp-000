b3811c2880685a8598cd89ec8a939067366615fd
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-21 21:30:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here
+
+class Shoe 
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  
+  def initialize(title)
+    @title = title
+    @page_count = 0
+  end
